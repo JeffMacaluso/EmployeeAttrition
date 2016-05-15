7451406ea5f828e8b3150d8bc9e12cab2d92e926
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2016-05-15 11:27:59 -0500

    Update Distance Calculation.sql

--- Distance Calculation.sql
+++ Distance Calculation.sql
@@ -1,13 +1,13 @@
 -- Determines the distance between an employee's address and primary store in miles using geocodes
 
-WITH Employee AS (
+WITH Employees AS (
 SELECT
  EmployeeKey
 , HomeStore_DimOrganizationStructureStoreKey
 , Longitude
 , Latitude
-FROM FlightRisk
-  JOIN Employee ON FlightRisk.EmployeeKey = Employee.DimOperationalStructureEmployeeKey
+FROM dbo.FlightRisk
+  JOIN dbo.Employee ON FlightRisk.EmployeeKey = Employee.DimOperationalStructureEmployeeKey
 WHERE Longitude IS NOT NULL AND Latitude IS NOT NULL
 )
 
@@ -17,15 +17,15 @@ SELECT
 , OrgStoreNumber
 , Longitude
 , Latitude
-FROM Store 
+FROM dbo.Store 
 WHERE Longitude IS NOT NULL AND Latitude IS NOT NULL
 )
 
 SELECT 
  EmployeeKey
-, (Geography::Point(Employee.Latitude, Employee.Longitude, 4326).STDistance(Geography::Point(Stores.LATITUDE, Stores.LONGITUDE, 4326)))*(0.00062137) as Distance_Miles
+, (Geography::Point(Employees.Latitude, Employees.Longitude, 4326).STDistance(Geography::Point(Stores.LATITUDE, Stores.LONGITUDE, 4326)))*(0.00062137) as Distance_Miles
 
-FROM Employee
-  JOIN Stores ON Employee.HomeStore_DimOrganizationStructureStoreKey = Stores.DimOrganizationStructureStoreKey
+FROM Employees
+  JOIN Stores ON Employees.HomeStore_DimOrganizationStructureStoreKey = Stores.DimOrganizationStructureStoreKey
 WHERE 
-(Geography::Point(Employee.Latitude, Employee.Longitude, 4326).STDistance(Geography::Point(Stores.LATITUDE, Stores.LONGITUDE, 4326)))*(0.00062137) < 100
+(Geography::Point(Employees.Latitude, Employees.Longitude, 4326).STDistance(Geography::Point(Stores.LATITUDE, Stores.LONGITUDE, 4326)))*(0.00062137) < 100
